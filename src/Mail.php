4d14553c10f83636ad686a1f9d574bf2ad54af47
krystiano13 <crystianotv@gmail.com>
2023-12-12 11:44:41 +0100

    add data for smtp

--- src/Mail.php
+++ src/Mail.php
@@ -11,9 +11,13 @@ class Mail
     private static string $host = "smtp.gmail.com";
     private static int $port = 465;
     private PHPMailer $mailer;
+    private string $email;
+    private string $secret;
 
     public function __construct()
     {
         $this -> mailer = new PHPMailer(true);
+        $this -> email = $_ENV['email'];
+        $this -> secret = $_ENV['secret'];
     }
 }
