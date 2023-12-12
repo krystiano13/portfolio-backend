ad64124bca3bf1f1173b63eb82a7c30b057c44ec
krystiano13 <crystianotv@gmail.com>
2023-12-12 11:39:11 +0100

    create Mail class

--- src/Mail.php
+++ src/Mail.php
@@ -8,5 +8,12 @@ use PHPMailer\PHPMailer\Exception;
 
 class Mail
 {
+    private static string $host = "smtp.gmail.com";
+    private static int $port = 465;
+    private PHPMailer $mailer;
 
+    public function __construct()
+    {
+        $this -> mailer = new PHPMailer(true);
+    }
 }
